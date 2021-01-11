6c546a8dbcc7eae98506e797c6b649b80d8a41c1
Tim Krüger <tim.krueger1805@googlemail.com>
2021-01-11 18:50:36 +0100

    Fixed typo

--- libs/engine-core/general/utility.c
+++ libs/engine-core/general/utility.c
@@ -2,7 +2,7 @@
 
 void utility_vec3_mulsubs(vec3 a, float s, vec3 dest) 
 {
-  dest[0] += a[0] * s;
-  dest[1] += a[1] * s;
-  dest[2] += a[2] * s;
+  dest[0] -= a[0] * s;
+  dest[1] -= a[1] * s;
+  dest[2] -= a[2] * s;
 }
